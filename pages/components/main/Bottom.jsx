44b19caf8139854689d3237eff0ef6264d9a08e4
parsa ardalan <parsardlan2112@gmail.com>
2025-08-22 17:54:55 +0330

    bottom part of home page has been programmed.

--- pages/components/main/Bottom.jsx
+++ pages/components/main/Bottom.jsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 export default function Bottom() {
 
     return (
 
-        <div className="w-full h-1/6 pt-5 border">
+        <div className="w-full h-1/6 px-5" dir="ltr">
 
-            <h1 className="text-lg text-center text-black"> bottom part </h1>
+            <Link href={"#"} >
+
+                <h3 className="text-lg text-shadow-xs text-shadow-black/20"> مشاهده نمونه کارها </h3>
+
+            </Link>
 
         </div>
     )
