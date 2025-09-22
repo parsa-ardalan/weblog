ab57e9ffe2de03bad1cedaddac6764d78217f05f
parsa ardalan <parsardlan2112@gmail.com>
2025-09-22 21:19:17 +0330

    style changes in page resume.

--- pages/components/resume/Skills.jsx
+++ pages/components/resume/Skills.jsx
@@ -2,9 +2,9 @@ export default function Skills() {
 
     return (
         
-        <div className="col-span-1 bg-white/10 rounded-2xl p-4">
+        <div className="col-span-1 rounded-2xl p-4">
 
-            <h1 className="text-xl text-white text-shadow-sm text-shadow-black"> مهارت های اصلی</h1>
+            <h1 className="text-xl text-white text-shadow-sm text-shadow-black"> مهارت های اصلی </h1>
 
             <p dir="rtl" className="text-sm text-white/70 text-shadow-sm text-shadow-black mt-3 leading-8">
 
--- pages/resume/index.jsx
+++ pages/resume/index.jsx
@@ -11,7 +11,7 @@ export default function Resume() {
 
             <Header />
 
-            <div className="container mx-auto grid grid-cols-1 gap-y-3 py-6 px-4 bg-black-10 backdrop-blur-xs border border-white rounded-xl w-3/4 h-3/5 mt-36 overflow-y-scroll">
+            <div className="container mx-auto grid grid-cols-1 gap-y-3 py-3 px-4 bg-black-10 backdrop-blur-xs border border-white rounded-xl w-3/4 h-3/5 mt-36 overflow-y-scroll">
 
                 {/* text info and skills */}
 
