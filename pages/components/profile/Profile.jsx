5f43bcc93b134b2a870249dcdc96b41648e06d40
parsa ardalan <parsardlan2112@gmail.com>
2025-10-09 18:17:54 +0330

    style changed, component profile, info removed.

--- pages/components/profile/Profile.jsx
+++ pages/components/profile/Profile.jsx
@@ -16,24 +16,6 @@ export default function Profile() {
             <h1 className="text-lg text-white/70 text-center text-shadow-sm text-shadow-black mt-4"> {data.personalInfo.name} </h1>
             <h2 className="text-md text-white/70 text-center text-shadow-sm text-shadow-black mt-2"> {data.personalInfo.jobTitle} </h2>
 
-            {/* _____________________ */}
-            <div className="flex justify-center">
-                <hr className="text-black mt-5 w-4/5 text-center" />
-            </div>
-
-            {/* info */}
-            <div className="w-4/5 h-2/6 mx-auto mt-3">
-
-                <p className="text-sm text-white/70 text-right text-shadow-sm text-shadow-black">
-                    دارای مدرک تخصصی کامپیوتر و برنامه نویسی(CS50x) از دانشگاه عالی رتبه هاروارد.
-                </p>
-
-                <p className="text-sm text-white/70 text-right text-shadow-sm text-shadow-black">
-                    سه سال تجربه برنامه نویسی صفحات مدرن و واکنش گرا.
-                </p>
-
-            </div>
-
-        </div >
+        </div>
     )
 }
